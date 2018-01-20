567581300d9a4c1daf37ef69684b30d0a51ae8aa
Alexis Santo <alexis.santo@epitech.eu>
2018-01-20 15:12:05 +0100

    changes
    cleaning

--- mqtt_esp8266.ino
+++ mqtt_esp8266.ino
@@ -1,31 +1,8 @@
-/*
- Basic ESP8266 MQTT example
-
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
-
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
-
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
-
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-
-*/
-
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+WiFiClient espClient;
+PubSubClient client(espClient);
 // Update these with values suitable for your network. 
 const char* ssid = "PX-0.1";
 const char* password = "557744Aa";
@@ -34,15 +11,12 @@ const char* mqtt_server = "192.168.43.56";
 const char* lightChannel = "/ESP8266/sensors/light_sensor";
 const char* moistureChannel = "/ESP8266/sensors/moisture_sensor";
 const char* logChannel = "/ESP8266/log";
-const char* testChannel = "/ESP8266/test";
+const char* blueLedChannel = "/ESP8266/blueLed";
+const char* orangeLedChannel = "/ESP8266/orangeLed";
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-long lastMessage = 0;
-char message[50];
 int value = 0;
 int dots = 0;
-
+bool isSub = false;
 
 int moistureSensorPin = A0;  
 int moistureSensorValue = 0;  
@@ -82,6 +56,7 @@ void setup_wifi()
       dots = 0;
     }
     Serial.print(".");
+    dots++;
   }
 
   Serial.println("");
@@ -92,17 +67,22 @@ void setup_wifi()
 
 void callback(char* topic, byte* payload, unsigned int length)
 {
-  Serial.print("Message arrived [");
+  Serial.print("\n\nMessage arrived [");
   Serial.print(topic);
   Serial.print("] ");
+  char* message = "";
+    
   for (int i = 0; i < length; i++)
   {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
+  char* msg = strcat("message : ", message);
+  Serial.print(msg);
+  int nb = atoi(message);
+  
   // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1')
+  if (nb < 10)
   {
     digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
     // but actually the LED is on; this is because
@@ -126,7 +106,7 @@ void reconnect()
     {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish(logChannel, "Wemos D1 mini pro connected on server");
+      client.publish(logChannel, "Wemos D1 mini pro connected");
     }
     else
     {
@@ -158,14 +138,16 @@ int convertToPercent(int value)
 {
   int percentValue = 0;
   percentValue = map(value, 1023, 465, 0, 100);
-  return percentValue;
+  return (percentValue);
 }
 
 void lightSensorChecking()
 {
-  lightSensorValue = analogRead(lightSensorPin);
   char* valueToPublish = "light sensor : ";
+  
+  lightSensorValue = analogRead(lightSensorPin);
   sprintf(valueToPublish, "%d", lightSensorValue);
+  
   client.publish(lightChannel, valueToPublish);
   printLightValuesToSerial();
   delay(1000);
@@ -173,11 +155,13 @@ void lightSensorChecking()
 
 void moistureSensorChecking()
 {
+  char* valueToPublish = "moisture sensor : ";
+  
   moistureSensorValue = analogRead(moistureSensorPin);
   moisturePercent = convertToPercent(moistureSensorValue);
-  char* valueToPublish = "moisture sensor : ";
   sprintf(valueToPublish, "%d", moisturePercent);
-  valueToPublish = strcat(valueToPublish, "%");
+  //valueToPublish = strcat(valueToPublish, "%");
+  
   client.publish(moistureChannel, valueToPublish);
   printMoistureValuesToSerial();
   delay(1000);
@@ -191,6 +175,11 @@ void loop()
   }
   client.loop();
   //lightSensorChecking();
+  if (isSub == false)
+  {
+      client.subscribe(moistureChannel);
+      isSub = true; 
+  }
   delay(3000);
   moistureSensorChecking();
 }
